813893585b95ebe1c703f5ea7c8411e79f1c108f
buckees <67809187+buckees@users.noreply.github.com>
2020-11-25 22:40:08 -0800

    Create test_mesh1d.py

---
+++ tests/test_mesh1d.py
@@ -0,0 +1,26 @@
+"""Test Geom1d."""
+
+import os
+import glob
+for i in glob.glob("*.png"):
+    os.remove(i)
+
+from Geom_and_Mesh.LngmrMod_Geom import RctMod1D, Domain1D, Interval
+from Geom_and_Mesh.LngmrMod_Mesh import Mesh1D
+
+ICP1d = RctMod1D(name='ICP1D', is_cyl=False)
+domain1d = Domain1D(domain=(-10.0, 10.0))
+ICP1d.add_domain(domain1d)
+seg1 = Interval('M', (-10.0, -8.0))
+ICP1d.add_shape(seg1)
+seg2 = Interval('M', (5.0, 10.0))
+ICP1d.add_shape(seg2)
+seg3 = Interval('D', (-6.0, 0.0))
+ICP1d.add_shape(seg3)
+ICP1d.plot()
+print(ICP1d)
+
+# generate mesh to imported geometry
+mesh1d = Mesh1D(import_geom=ICP1d)
+mesh1d.gen_mesh(nx=100)
+mesh1d.plot()
