7ca1451f661e36687e2359e74ee80ab08926aa9b
buckees <67809187+buckees@users.noreply.github.com>
2020-12-09 20:24:48 -0800

    Deleta all plot()

--- packages/Reactor2D/Reactor2D_Plasma.py
+++ packages/Reactor2D/Reactor2D_Plasma.py
@@ -9,14 +9,8 @@ PLASMA2D contains:
 """
 
 import numpy as np
-from copy import copy, deepcopy
-import matplotlib.pyplot as plt
-import matplotlib.patches as patch
-import matplotlib.cm as cm
-colMap = copy(cm.get_cmap("jet"))
-colMap.set_under(color='white')
 
-from packages.Constants import (AMU, UNIT_CHARGE, EON_MASS, COLOR_DICT)
+from packages.Constants import (AMU, UNIT_CHARGE, EON_MASS)
 
 class PLASMA2D(object):
     """Define PLASMA2D."""
@@ -104,109 +98,8 @@ class PLASMA2D(object):
         self.ni = np.clip(self.ni, n_min, n_max)
         self.nn = np.clip(self.nn, n_min, n_max)
         self.Te = np.clip(self.Te, T_min, T_max)
-        self.Ti = np.clip(self.Ti, T_min, T_max)
-
-    def plot(self, figsize=(8, 8), ihoriz=1, dpi=300, 
-             fname='Plasma.png', imode='Contour', iplot_geom=0):
-        """
-        Plot plasma variables vs. position.
-            
-        var include density, temperature.
-        figsize: a.u., (2, ) tuple, size of fig
-        ihoriz: a.u., var, 0 or 1, set the layout of fig horizontal or not
-        dpi: a.u., dots per inch
-        fname: str, var, name of png file to save
-        imode: str, var, ['Contour', 'Scatter']
-        """
-        _x, _z = self.mesh.x, self.mesh.z
-        if ihoriz:
-            fig, axes = plt.subplots(1, 2, figsize=figsize, dpi=dpi,
-                                     constrained_layout=True)
-        else:
-            fig, axes = plt.subplots(2, 1, figsize=figsize, dpi=dpi,
-                                     constrained_layout=True)
-        
-        # plot densities
-        if imode == 'Contour':
-            for _ax, _den, _title in zip(axes, (self.ne, self.ni), 
-                                         ('E Density', 'Ion Density')):
-                _cs = _ax.contourf(_x, _z, _den, cmap=colMap, vmin=1.1e11)
-                _ax.set_title(_title)
-                fig.colorbar(_cs, ax=_ax, shrink=0.9)
-            
-        elif imode == 'Scatter':
-            for _ax, _den, _title in zip(axes, (self.ne, self.ni), 
-                                         ('E Density', 'Ion Density')):
-                _ax.scatter(_x, _z, c=_den, s=5, cmap=colMap, vmin=1.1e11)
-                _ax.set_title(_title)
-            
-        for ax in axes:
-            ax.set_xlabel('Position (m)')
-            ax.set_ylabel('Height (m)')
-            ax.set_aspect('equal')
-            
-            # add geom plot
-            if iplot_geom:
-                for shape in self.mesh.geom.sequence:
-                    if shape.type == 'Rectangle':
-                        temp_col = COLOR_DICT[self.mesh.geom.label[shape.label]]
-                        ax.add_patch(
-                            patch.Rectangle(shape.bl, shape.width, shape.height,
-                                            facecolor=temp_col, edgecolor='w')
-                            )
-        
-        fig.savefig(fname, dpi=dpi)
-        plt.close()
+        self.Ti = np.clip(self.Ti, T_min, T_max)    
     
-    def get_Te(self, een):
-        """
-        Get Te from Eergy2d().
-        
-        een: Eergy2d() boject.
-        """
-        self.Te = deepcopy(een.Te)
-    
-    def plot_Te(self, figsize=(8, 8), ihoriz=1, 
-                    dpi=300, fname='Te.png', imode='Contour'):
-        """
-        Plot plasma variables vs. position.
-            
-        var include density, temperature.
-        figsize: a.u., (2, ) tuple, size of fig
-        ihoriz: a.u., var, 0 or 1, set the layout of fig horizontal or not
-        dpi: a.u., dots per inch
-        fname: str, var, name of png file to save
-        imode: str, var, ['Contour', 'Scatter']
-        """
-        _x, _z = self.mesh.x, self.mesh.z
-        if ihoriz:
-            fig, axes = plt.subplots(1, 2, figsize=figsize, dpi=dpi,
-                                     constrained_layout=True)
-        else:
-            fig, axes = plt.subplots(2, 1, figsize=figsize, dpi=dpi,
-                                     constrained_layout=True)
-        
-        # plot densities
-        if imode == 'Contour':
-            for _ax, _den, _title in zip(axes, (self.Te, self.Ti), 
-                                ('E Temperature', 'Ion Temperature')):
-                _cs = _ax.contourf(_x, _z, _den, cmap=colMap)
-                _ax.set_title(_title)
-                fig.colorbar(_cs, ax=_ax, shrink=0.9)
-            
-        elif imode == 'Scatter':
-            for _ax, _den, _title in zip(axes, (self.Te, self.Ti), 
-                                ('E Temperature', 'Ion Temperature')):
-                _ax.scatter(_x, _z, c=_den, s=5, cmap=colMap)
-                _ax.set_title(_title)
-            
-        for ax in axes:
-            ax.set_xlabel('Position (m)')
-            ax.set_ylabel('Height (m)')
-            ax.set_aspect('equal')
-        fig.savefig(fname, dpi=dpi)
-        plt.close()
-
     def update_plasma(self):
         """
         Make sure the all attr in PLASMA2D are updated.
@@ -231,43 +124,6 @@ class PLASMA2D(object):
             if mat == 2:
                 self.eps[idx] = 3.8
         
-    def plot_eps(self, figsize=(8, 8), ihoriz=1, 
-                    dpi=300, fname='eps.png', imode='Contour'):
-        """
-        Plot eon conductivity vs. position.
-            
-        var include density, temperature.
-        figsize: a.u., (2, ) tuple, size of fig
-        ihoriz: a.u., var, 0 or 1, set the layout of fig horizontal or not
-        dpi: a.u., dots per inch
-        fname: str, var, name of png file to save
-        imode: str, var, ['Contour', 'Scatter']
-        """
-        x, z = self.mesh.x, self.mesh.z
-        if ihoriz:
-            fig, axes = plt.subplots(1, 2, figsize=figsize, dpi=dpi,
-                                     constrained_layout=True)
-        else:
-            fig, axes = plt.subplots(2, 1, figsize=figsize, dpi=dpi,
-                                     constrained_layout=True)
-        
-        # plot eon conductivity
-        for ax, var, title in zip(axes, (self.eps, self.eps), 
-                        ('Dielectric Constant', 'Dielectric Constant')):
-            if imode == 'Contour':
-                cs = ax.contourf(x, z, var, cmap=colMap)
-            elif imode == 'Scatter':
-                cs = ax.scatter(x, z, c=var, s=5, cmap=colMap)
-            ax.set_title(title)
-            fig.colorbar(cs, ax=ax, shrink=0.9)
-        
-        for ax in axes:
-            ax.set_xlabel('Position (m)')
-            ax.set_ylabel('Height (m)')
-            ax.set_aspect('equal')
-        fig.savefig(fname, dpi=dpi)
-        plt.close()
-
     def _calc_conde(self):
         """Calc eon conductivity."""
         temp_conde = UNIT_CHARGE**2/EON_MASS
@@ -276,40 +132,3 @@ class PLASMA2D(object):
         self.conde += self.coll_em
         self.conde += complex(0.0, self._wrf)
         self.conde = np.divide(temp_conde, self.conde)
-    
-    def plot_conde(self, figsize=(8, 8), ihoriz=1, 
-                    dpi=300, fname='conde.png', imode='Contour'):
-        """
-        Plot eon conductivity vs. position.
-            
-        var include density, temperature.
-        figsize: a.u., (2, ) tuple, size of fig
-        ihoriz: a.u., var, 0 or 1, set the layout of fig horizontal or not
-        dpi: a.u., dots per inch
-        fname: str, var, name of png file to save
-        imode: str, var, ['Contour', 'Scatter']
-        """
-        x, z = self.mesh.x, self.mesh.z
-        if ihoriz:
-            fig, axes = plt.subplots(1, 2, figsize=figsize, dpi=dpi,
-                                     constrained_layout=True)
-        else:
-            fig, axes = plt.subplots(2, 1, figsize=figsize, dpi=dpi,
-                                     constrained_layout=True)
-        
-        # plot eon conductivity
-        for ax, var, title in zip(axes, (self.conde.real, self.conde.imag), 
-                        ('E conductivity REAL', 'E conductivity IMAG')):
-            if imode == 'Contour':
-                cs = ax.contourf(x, z, var, cmap=colMap)
-            elif imode == 'Scatter':
-                cs = ax.scatter(x, z, c=var, s=5, cmap=colMap)
-            ax.set_title(title)
-            fig.colorbar(cs, ax=ax, shrink=0.9)
-        
-        for ax in axes:
-            ax.set_xlabel('Position (m)')
-            ax.set_ylabel('Height (m)')
-            ax.set_aspect('equal')
-        fig.savefig(fname, dpi=dpi)
-        plt.close()
