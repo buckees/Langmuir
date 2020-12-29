e0e726fbd95eb9a6cd9949fcfdfc8493d620151c
buckees <67809187+buckees@users.noreply.github.com>
2020-12-28 23:15:10 -0800

    Add mp.plot()

--- packages/Model/Common/Utest_gen_particles.py
+++ packages/Model/Common/Utest_gen_particles.py
@@ -12,5 +12,4 @@ Arp = {'name':'Ar+','type':'Ion', 'mass':40.0, 'charge':1.0}
 posn = np.random.rand(num, 3)
 vel = np.random.rand(num, 3)
 mp.gen_particles(num=100, prop=Arp, posn=posn, vel=vel)
-
-print(mp)
+mp.plot()
