db233245781d2b34e69d83173a2936cd955d135a
buckees <67809187+buckees@users.noreply.github.com>
2020-12-29 20:04:45 -0800

    Update __init___()

--- packages/Model/Common/Particle.py
+++ packages/Model/Common/Particle.py
@@ -11,11 +11,27 @@ class PARTICLE(object):
 
     def __init__(self, name='Particle'):
         """
-        Init the PARTICLE.
+        Init the PARTICLE().
         
-        name: str, var, name of the PARTICLE.
+        name: str, name of the PARTICLE.
+        pname: str, name of particle
+        ptype: str, type of particle, ['Eon', 'Ion', 'Neut']
+        mass: float, mass of particle, unit in AMU
+        charge: float, charge of particle
+        isAlive: bool, state of particle
+        posn: arr(3) of float, position of particle, unit in m
+        vel: arr(3) of float, velocity of particle, unit in m/s
+        erg: float, energy of particle, unit in eV
         """
         self.name = name
+        self.pname = list()
+        self.ptype = list()
+        self.mass = np.array(list())
+        self.charge = np.array(list())
+        self.isAlive = np.array(list())
+        self.posn = np.array(list())
+        self.vel = np.array(list())
+        self.erg = np.array(list())
      
     def read_species(self, fname):
         """Read in species info from a database (Species.csv)."""
