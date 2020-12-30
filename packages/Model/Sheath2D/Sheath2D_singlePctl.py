1b1138c42dddba8c9edba31c1b770a6af48b6339
buckees <67809187+buckees@users.noreply.github.com>
2020-12-30 10:42:18 -0800

    Replace EF calc with FIELD()

--- packages/Model/Sheath2D/Sheath2D_singlePctl.py
+++ packages/Model/Sheath2D/Sheath2D_singlePctl.py
@@ -5,6 +5,7 @@ from math import sin
 import matplotlib.pyplot as plt
 
 from packages.Model.Common.Particle import PARTICLE
+from packages.Model.Common.Field import FIELD
 from packages.Constants import (PI, AMU, UNIT_CHARGE)
 
 # Physics parameters
@@ -12,10 +13,14 @@ w_loc = 0.0 # by default
 
 dsh = 0.01
 dt = 1e-8
-Vdc=100
-Vrf=50
+Vdc, Vrf = np.zeros(3), np.zeros(3)
+Vdc[1], Vrf[1] = 100.0, 30.0
 # freq = 13.56e6 # T = 70 ns
 freq = 1
+
+def Efunc(Vdc, Vrf, dsh, freq, phi0, t):
+    """Calc sheath E-field."""
+    return -Vdc/dsh - Vrf/dsh*sin(2*PI*freq*t + phi0)
 
 # Model parameters
 num_ptcl = 10000
@@ -23,6 +28,9 @@ max_step = 10000
 
 ptcl = PARTICLE('M+')
 ptcl.customize_ptcl('Ion', 40, 1)
+
+field = FIELD('Sheath')
+field.add_Efunc(Efunc)
 
 erg = list()
 
@@ -43,11 +51,10 @@ for i in range(num_ptcl):
             if dt1 < dt:
                 dt = dt1*1.001
         # move
-        EF = np.zeros(3)
-        EF[1] = -Vdc/dsh - Vrf/dsh*sin(2*PI*freq*t + phi0)
+        field.E = field.Efunc(Vdc, Vrf, dsh, freq, phi0, t)
         ptcl.update_posn(ptcl.posn + ptcl.vel*dt)
         ptcl.update_vel(ptcl.vel + 
-                        EF*(ptcl.charge*UNIT_CHARGE)/(ptcl.mass*AMU)*dt)
+                        field.E*(ptcl.charge*UNIT_CHARGE)/(ptcl.mass*AMU)*dt)
         t += dt
         step += 1
         if ptcl.posn[1] < w_loc:
@@ -63,10 +70,10 @@ print(f'{num_ptcl} particles are launched.'
 
 fig, ax = plt.subplots(1, 1, figsize=(4, 3), dpi=600,
                        constrained_layout=True)
-ax.hist(erg, 2*int(Vrf), density=True)
+ax.hist(erg, 2*int(Vrf[1]), density=True)
 ax.set_xlim(40, 160)
 ax.set_title('Ion Energy Distribution')
 ax.set_xlabel('Energy (eV)')
 ax.set_ylabel('Count')
-fig.savefig(f'Vdc{Vdc}_Vrf{Vrf}.png', dpi=600)
+fig.savefig(f'Vdc{Vdc[1]}_Vrf{Vrf[1]}.png', dpi=600)
 plt.close()
