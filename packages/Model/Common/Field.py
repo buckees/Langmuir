2d22b4af178c92eba961a794540b89cb6ce70603
buckees <67809187+buckees@users.noreply.github.com>
2020-12-30 10:23:55 -0800

    Create Field.py

---
+++ packages/Model/Common/Field.py
@@ -0,0 +1,34 @@
+"""
+Field.py serves as a data center/hub,
+    controls input and output of E and B,
+    stores E and B,
+    is shered by all particle-based models,
+    supports single and multi particle.
+"""
+
+import numpy as np
+
+from packages.Constants import (PI, AMU, UNIT_CHARGE, EV2J, J2EV)
+
+class FIELD(object):
+    """Create FIELD() object."""
+
+    def __init__(self, name='Field'):
+        """
+        Init the FIELD().
+        
+        name: str, name of the field.
+        E: arr(3) of float, E-field in (x, z, y), unit in V/m
+        B: arr(3) of float, B-field in (x, z, y), unit in T
+        """
+        self.name = name
+        self.E = np.zeros(3)
+        self.B = np.zeros(3)
+     
+    def add_Efunc(self, Efunc):
+        """
+        Add function for E-field.
+        
+        Efunc: function which determines E-field
+        """
+        self.Efunc = Efunc
