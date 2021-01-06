a348f9a02ca51f30c33a09fa59b94a6d52b9971c
buckees <67809187+buckees@users.noreply.github.com>
2021-01-05 22:45:54 -0800

    Update with MAIN()

--- packages/Model/Sheath2D/Sheath2D_main.py
+++ packages/Model/Sheath2D/Sheath2D_main.py
@@ -14,14 +14,15 @@ w_loc = 0.0 # by default
 
 dsh = 0.01
 dt = 1e-8
-Vdc, Vrf = np.zeros(3), np.zeros(3)
-Vdc[1], Vrf[1] = 100, 20
+Vdc, Vrf = 100, 20
 # freq = 13.56e6 # T = 70 ns
 freq = 1
 
 def Efunc(Vdc, Vrf, dsh, freq, phi0, t):
     """Calc sheath E-field."""
-    return -Vdc/dsh - Vrf/dsh*sin(2*PI*freq*t + phi0)
+    E = np.zeros(3)
+    E[1] = -Vdc/dsh - Vrf/dsh*sin(2*PI*freq*t + phi0)
+    return E
 
 # Model parameters
 num_ptcl = 10000
@@ -33,48 +34,64 @@ ptcl.customize_ptcl('Ion', 40, 1)
 field = FIELD('Sheath')
 field.add_Efunc(Efunc)
 
-erg = list()
 
-for i in range(num_ptcl):
-    # init
-    ptcl.update_state(True)
-    ptcl.update_posn(np.array([0.0, dsh, 0.0]))
-    ptcl.update_vel(np.zeros(3))
-    phi0 = np.random.uniform(0.0, 2*PI)
-    t = 0
-    dt = 1e-8
-    step = 0
+
+def MAIN(oper, ptcl, coll=None):
+    """
+    MAIN() actually runs the feature model.
+    oper: OPERATION(obj), contains all operation parameters.
+    ptcl: PARTICLE(obj), contains all particle information.
+    coll: COLLISION(obj), contains all collision information.
+    """
+
+    erg = list()
+    for i in range(oper.num_ptcl):
+        
+        ########## init ptcl ##########
+        ptcl.update_state(True)
+        ptcl.update_posn(np.array([0.0, oper.d_sh, 0.0]))
+        ptcl.update_vel(np.zeros(3))
+        ################################
+        
+        dt = oper.dt
+        t = 0
+        step = 0
+        phi0 = np.random.uniform(0.0, 2*PI)
+        
+        ###############################################
+        ########## main loop for pctl launch ##########
+        ###############################################
+        while ptcl.isAlive:
+            # make sure not over shoot
+            if ptcl.vel[1]:
+                dt1 = (oper.wfr_loc - ptcl.posn[1])/ptcl.vel[1]
+                if dt1 < dt:
+                    dt = dt1*1.001
+            # move
+            field.update_E(field.Efunc(oper.Vdc, oper.Vrf, oper.d_sh, 
+                                       oper.freq, phi0, t))
+            posn_next, vel_next = EULER_MOVE(ptcl, field, dt)
+            ptcl.update_posn(posn_next)
+            ptcl.update_vel(vel_next)
+            t += dt
+            step += 1
+            if ptcl.posn[1] < oper.wfr_loc:
+                ptcl.update_state(False)
+                erg.append(ptcl.vel2erg())
+            if step > max_step:
+                ptcl.update_state(False)
     
-    while ptcl.isAlive:
-        # make sure not over shoot
-        if ptcl.vel[1]:
-            dt1 = (w_loc - ptcl.posn[1])/ptcl.vel[1]
-            if dt1 < dt:
-                dt = dt1*1.001
-        # move
-        field.update_E(field.Efunc(Vdc, Vrf, dsh, freq, phi0, t))
-        posn_next, vel_next = EULER_MOVE(ptcl, field, dt)
-        ptcl.update_posn(posn_next)
-        ptcl.update_vel(vel_next)
-        t += dt
-        step += 1
-        if ptcl.posn[1] < w_loc:
-            ptcl.isAlive=False
-            erg.append(ptcl.vel2erg())
-        if step > max_step:
-            ptcl.isAlive=False
-
-# plot results
-print(f'{num_ptcl} particles are launched.' 
-      + f'\n{len(erg)} particles are collected by the wafer.')
-
-
-fig, ax = plt.subplots(1, 1, figsize=(4, 3), dpi=600,
-                       constrained_layout=True)
-ax.hist(erg, 2*int(Vrf[1]), density=True)
-ax.set_xlim(40, 160)
-ax.set_title('Ion Energy Distribution')
-ax.set_xlabel('Energy (eV)')
-ax.set_ylabel('Count')
-fig.savefig(f'Vdc{Vdc[1]}_Vrf{Vrf[1]}.png', dpi=600)
-plt.close()
+    # plot results
+    print(f'{num_ptcl} particles are launched.' 
+          + f'\n{len(erg)} particles are collected by the wafer.')
+    
+    
+    fig, ax = plt.subplots(1, 1, figsize=(4, 3), dpi=600,
+                           constrained_layout=True)
+    ax.hist(erg, 2*int(Vrf), density=True)
+    ax.set_xlim(40, 160)
+    ax.set_title('Ion Energy Distribution')
+    ax.set_xlabel('Energy (eV)')
+    ax.set_ylabel('Count')
+    fig.savefig(f'Vdc{oper.Vdc}_Vrf{oper.Vrf}.png', dpi=600)
+    plt.close()
