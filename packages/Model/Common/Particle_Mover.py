7ebf493d21fd137a8be748c709b115a7c5d35061
buckees <67809187+buckees@users.noreply.github.com>
2021-01-06 10:48:51 -0800

    Add S_EULER()

--- packages/Model/Common/Particle_Mover.py
+++ packages/Model/Common/Particle_Mover.py
@@ -13,7 +13,7 @@ def EULER_MOVE(ptcl, field, dt):
     Solve the Newton's equatoin of motion by Euler method.
     
     x(t + dt) = x(t) + v(t) * dt
-    v(t + dt) = v(t) + F/m * dt
+    v(t + dt) = v(t) + F(t)/m * dt
     x: arr(3) of float, position in (x, z, y)
     v: arr(3) of float, velocity in (x, z, y)
     ptcl: PARTICLE() or MULTI_PARTICLE() obj
@@ -21,4 +21,15 @@ def EULER_MOVE(ptcl, field, dt):
     """
     posn = ptcl.posn + ptcl.vel*dt
     vel = ptcl.vel + field.E*(ptcl.charge*UNIT_CHARGE)/(ptcl.mass*AMU)*dt
+    return posn, vel
+
+def S_EULER(ptcl, field, dt):
+    """
+    Semi-explicit Euler: 1st order symplectic integrator.
+    
+    v(t + dt) = v(t) + F(t)/m * dt
+    x(t + dt) = x(t) + v(t + dt) * dt
+    """    
+    vel = ptcl.vel + field.E*(ptcl.charge*UNIT_CHARGE)/(ptcl.mass*AMU)*dt
+    posn = ptcl.posn + ptcl.vel*dt
     return posn, vel
