74ca9db390d4dfd6adfbbec00e46f05fc0e1d06d
buckees <67809187+buckees@users.noreply.github.com>
2021-01-05 17:40:22 -0800

    Create PARAMETER()

--- packages/Model/Feature2D/Feature2D_ops.py
+++ packages/Model/Feature2D/Feature2D_ops.py
@@ -1,33 +1,17 @@
 """Feature Model 2D. Operation parameters."""
 
-# mesh informaiton
-width, height = 100.0, 260.0  # nm
-res_x, res_z = 1.0, 1.0  # nm
-
-# particle information
-num_ptcl = 10000
-# max steps for a single particle
-max_step = 1000
-# step length factor
-step_fac = 0.5
-
-
-# reflection information
-max_rflct = 5
-
-# reaction informaiton
-threshold = 1.0
-
-# init uvec distribution
-# idstrb = ['Uniform3D', -45.0, 45.0]
-idstrb = ['Mono', 0.0]
-
-# boundary condition
-ibc = 'periodic'
-
-# surf norm calc
-surf_norm_range = 4
-surf_norm_mode = 'Sum Vector'
-
-# diagnostics
-num_plot = 5
+class PARAMETER():
+    def __init__(self):
+        """
+        Init the PARTICLE().
+        
+        name: str, name of the particle.
+        """
+        self.num_ptcl = None
+        self.max_step = None
+        self.step_fac = None
+        self.max_rflct = None
+        self.surf_norm_range = None
+        self.surf_norm_mode = None
+        self.num_plot = None
+        self.prob_rflct = None
