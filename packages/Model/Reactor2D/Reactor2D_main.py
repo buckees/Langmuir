def4b9ae0e1290aa6a83cb5e6942367072bc386e
buckees <67809187+buckees@users.noreply.github.com>
2021-01-06 21:38:45 -0800

    Create Reactor2D_main.py

---
+++ packages/Model/Reactor2D/Reactor2D_main.py
@@ -0,0 +1,148 @@
+"""Sheath Model 2D. Main program."""
+
+import numpy as np
+
+def MAIN(oper, mesh, pla, txp, eergy=None, rct=None, field=None):
+    """
+    MAIN() actually runs the feature model.
+    oper: OPERATION(obj), contains all operation parameters.
+    pla: PLASMA2D(obj), contains all plasma parameters.
+    transp: TRANSP2D(obj), contains all transport information.
+    eergy: EERGY2D(obj), contains all collision information.
+    """
+
+    ########## init and plot plasma ##########
+    pla.import_mesh(mesh)
+    pla.init_plasma(ne=oper.ne, Te=oper.Te)
+    
+    temp_ratio = mesh.width/mesh.height
+    if temp_ratio > 2.0:
+        figsize = (4, 8)
+        ihoriz = 0
+    else:
+        figsize = (10, 4)
+        ihoriz = 1
+        
+    mesh.plot_var(var=[pla.ne, pla.ni], 
+                  var_name=['E Density', 'Ion Density'],
+                  fname='Init_Density.png')
+    mesh.plot_var(var=[pla.Te, pla.Ti], 
+                  var_name=['E Temperature', 'Ion Temperature'],
+                  fname='Init_Temperature.png')
+
+    ########## pre-run plasma ##########
+    txp.from_PLASMA(pla)
+    
+    for itn in range(oper.num_iter):
+        txp.calc_ambi(pla)
+        txp.solve_fluid(oper.dt)
+
+    txp.to_PLASMA(pla)
+    pla.update_plasma()
+    mesh.plot_var(var=[pla.ne, pla.ni], 
+                  var_name=['E Density', 'Ion Density'],
+                  fname='Prerun_Density.png')
+    mesh.plot_var(var=[pla.Te, pla.Ti], 
+                  var_name=['E Temperature', 'Ion Temperature'],
+                  fname='Prerun_Temperature.png')
+            
+    # txp.from_PLASMA(pla)
+    
+    # FIELD = FIELD2D('FIELD2D')
+    # FIELD.from_PLASMA(PLA)
+    # FIELD.create_Ey()
+    # FIELD.to_PLASMA(PLA)
+    
+    # MESH.plot_var(var=[PLA.Ey, PLA.Ex], 
+    #       var_name=['Ey', 'Ex'],
+    #       fname='E-Field')
+    
+    # init Eergy module
+    # EERN = EERGY2D('EERN2D')
+    
+    # dt = 2e-7
+    # niter = 10000
+    # EERN.from_PLASMA(PLA)
+    # for itn in range(niter):
+    #     EERN.solve_Te(PLA, dt)
+    #     if not (itn+1) % (niter/5):
+    #         EERN.to_PLASMA(PLA)
+    #         PLA.update_plasma()
+    #         MESH.plot_var(var=[PLA.Te, PLA.Ti], 
+    #               var_name=['E Temperature', 'Ion Temperature'],
+    #               fname=f'Te_itn{itn+1}')
+    #         MESH.plot_var(var=[PLA.pwr_in, EERN.dQe], 
+    #               var_name=['Power due to Ey', 'dQe'],
+    #               fname=f'Power_itn{itn+1}')
+    #         EERN.from_PLASMA(PLA)
+    # EERN.to_PLASMA(PLA)
+    
+    # # init React module
+    # SRC = REACT2D('SRC2D')
+    
+    
+    # ne_ave, ni_ave, Te_ave = [], [], []  
+    # time = []
+    # niter = 1000
+    # dt = 1e-7
+    # niter_Te = 40
+    
+    
+    # for itn in range(niter):
+    #     # call REACT2D
+    #     SRC.from_PLASMA(PLA)
+    #     SRC.calc_src(PLA)
+    #     SRC.to_PLASMA(PLA)
+    #     PLA.update_plasma()
+    #     # call TRANSP2D
+    #     TXP.from_PLASMA(PLA)
+    #     TXP.calc_ambi(PLA)
+    #     TXP.solve_fluid(dt)
+    #     TXP.to_PLASMA(PLA)
+    #     PLA.update_plasma()
+    #     # call EERGY2D
+    #     EERN.from_PLASMA(PLA)
+    #     for itn_Te in range(niter_Te):    
+    #         EERN.solve_Te(PLA, dt/niter_Te)
+    #     EERN.to_PLASMA(PLA)
+    #     PLA.update_plasma()
+    #     # record ave
+    #     ne_ave.append(deepcopy(PLA.ne_ave))
+    #     ni_ave.append(deepcopy(PLA.ni_ave))
+    #     Te_ave.append(deepcopy(PLA.Te_ave))
+    #     time.append(dt*(itn+1))
+    #     if not (itn+1) % (niter/5):
+    #         # plot 2D        
+    #         MESH.plot_var(var=[PLA.ne, PLA.ni], 
+    #               var_name=['E Density', 'Ion Density'],
+    #               fname=f'Density_itn{itn+1}')
+    #         MESH.plot_var(var=[PLA.Te, PLA.Ti], 
+    #               var_name=['E Temperature', 'Ion Temperature'],
+    #               fname=f'Te_itn{itn+1}')
+    #         MESH.plot_var(var=[PLA.dfluxe, PLA.Se], 
+    #               var_name=['E Loss', 'E Prod'],
+    #               fname=f'SRC_itn{itn+1}')
+    #         MESH.plot_var(var=[PLA.pwr_in, EERN.dQe], 
+    #               var_name=['Power due to Ey', 'dQe'],
+    #               fname=f'Power_itn{itn+1}')
+    
+    #         # plot ave. values
+    #         fig, axes = plt.subplots(1, 2, figsize=(8,4), dpi=300,
+    #                                               constrained_layout=True)
+    #         ax = axes[0]
+    #         ax.plot(time, ne_ave, 'b-')
+    #         ax.legend(['ne'])
+    #         ax.set_title('Eon Density (m^-3)')
+    #         plt.xlabel('Time (s)')
+    #         plt.ylabel('Ave. Density (m^-3)')
+            
+    #         ax = axes[1]
+    #         ax.plot(time, Te_ave, 'r-')
+    #         ax.legend(['Te'])
+    #         ax.set_title('Eon Temperature (eV)')
+    #         plt.xlabel('Time (s)')
+    #         plt.ylabel('Ave. Eon Temperature (eV)')
+            
+    #         fig.savefig('Ave_vs_Time.png', dpi=300)
+    #         plt.close()
+        
