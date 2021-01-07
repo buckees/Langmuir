d550be34d96789b2515d9bbcd5430ff1ee7ca87b
buckees <67809187+buckees@users.noreply.github.com>
2021-01-06 21:38:37 -0800

    Create Parameter.py

---
+++ packages/Model/Common/Parameter.py
@@ -0,0 +1,10 @@
+"""Feature Model 2D. Operation parameters."""
+
+class PARAMETER():
+    def __init__(self):
+        """
+        Init the PARTICLE().
+        
+        name: str, name of the particle.
+        """
+        self.num_iter = None
