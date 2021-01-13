221f70c880dd2b9ba60c678efc62647788b6f34d
buckees <67809187+buckees@users.noreply.github.com>
2021-01-13 13:19:37 -0800

    Add Ey_func()

--- run/Reactor2D/Reactor2D_run.py
+++ run/Reactor2D/Reactor2D_run.py
@@ -9,6 +9,8 @@ import glob
 for i in glob.glob("*.png"):
     os.remove(i)
 
+import numpy as np
+
 ########## import Langmuir modules ##########
 from packages.Model.Common.Yaml import PARAMETER
 from packages.Model.Reactor2D.Reactor2D_mesh import MESH2D
@@ -21,12 +23,13 @@ from packages.Model.Reactor2D.Reactor2D_main import MAIN
 
 # init operation parameters
 oper = PARAMETER()
-oper.num_iter = 1000
-oper.num_plot = 5
+oper.num_iter = 10000
+oper.num_plot = 10
 oper.dt = 1e-6
 oper.ne = 1e18
 oper.num_iter_Te = 30
 oper.Te = 2.0
+oper.input_pwr = 10.0
 oper.idiag = True
 
 # init mesh obj
@@ -45,6 +48,31 @@ eergy = EERGY2D('Eon_Energy')
 
 # init field module
 field = FIELD2D('Field')
+
+def Ey_func(x, z):
+    """
+    Produce E-field in y direction as a function of space.
+    
+    Ey(x, z) = Ey_const * (exp(-dist((x, z) to (x1, z1) )/decay_const) + 
+                           exp(-dist((x, z) to (x2, z2) )/decay_const))
+    x, z: 2d matrix 
+    Ey: 2d matrix as x and z, E-field in y direction.
+    """
+    Ey_const = 7e-1
+    decay_const = 0.1
+    x1, z1 = -0.12, 0.25
+    x2, z2 =  0.12, 0.25
+    dist1 = np.sqrt((x - x1)**2 + (z - z1)**2)
+    dist2 = np.sqrt((x - x2)**2 + (z - z2)**2)
+    Ey = Ey_const * (np.exp(-dist1/decay_const) + 
+                     np.exp(-dist2/decay_const))
+    return Ey
+
+field.add_Efunc(Ey_func, mesh)
+
+mesh.plot_var(var=[field.Ey, field.Ey], 
+                  var_name=['Ey', 'Ey'],
+                  fname='init_Ey.png')
 
 # init reaction module
 rct = REACT2D('React')
