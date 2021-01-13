4afa6d13c49832f708ce4fc95ec1bd2bfdef510a
buckees <67809187+buckees@users.noreply.github.com>
2021-01-12 21:30:53 -0800

    Add plot

--- run/Sheath2D/Sheath2D_run.py
+++ run/Sheath2D/Sheath2D_run.py
@@ -1,7 +1,11 @@
 """Sheath Model 2D. Main program."""
 
+import os
+import glob
+
 import numpy as np
 from math import sin
+import matplotlib.pyplot as plt
 
 from packages.Model.Common.Particle import PARTICLE
 from packages.Model.Common.Yaml import PARAMETER
@@ -18,15 +22,40 @@ oper.d_sh = 0.01
 oper.wfr_loc = 0.0
 oper.dt = 1e-8
 oper.Vdc = 100.0
-oper.Vrf = 20.0
-oper.freq = 1
+oper.Vrf = 50.0
+oper.freq = 2e6
+oper.iplot = False
+
+def Efunc_single(t):
+    d_sh = oper.d_sh
+    freq = oper.freq
+    Vdc = oper.Vdc
+    Vrf = oper.Vrf
+    return -Vdc/d_sh - Vrf/d_sh*sin(2*PI*freq*t)
+    
 
 # init ptcl
 ptcl = PARTICLE()
 ptcl.customize_ptcl('Ion', 40, 1)
 
 field = FIELD_SHEATH('Sheath')
-field.add_para(d_sh=oper.d_sh, Vdc=oper.Vdc, Vrf=oper.Vrf, 
-               freq=oper.freq, phi=0.0)
+field.add_Efunc(Efunc_single)
 
-MAIN(oper, ptcl, field, move=EULER_MOVE)
+erg, ang = MAIN(oper, ptcl, field, move=LEAPFROG)
+
+fname = f'freq{int(oper.freq/1e6)}_Vdc{int(oper.Vdc)}_Vrf{int(oper.Vrf)}'
+
+for i in glob.glob(fname + '.*'):
+    os.remove(i)
+
+np.save(fname, erg)
+
+fig, ax = plt.subplots(1, 1, figsize=(4, 3), dpi=600,
+                           constrained_layout=True)
+ax.hist(erg, bins=50, density=True)
+ax.set_title('Ion Energy Distribution')
+ax.set_xlabel('Energy (eV)')
+ax.set_ylabel('Count')
+fig.savefig(fname + '.png', dpi=600)
+plt.close()
+
