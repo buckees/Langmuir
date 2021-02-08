5e734b1a245c249204131070f55412df67b6482f
buckees <67809187+buckees@users.noreply.github.com>
2021-02-08 14:27:30 -0800

    Create Sheath2D_coll.py

---
+++ packages/Model/Sheath2D/Sheath2D_coll.py
@@ -0,0 +1,30 @@
+"""
+Sheath2D_coll.py contains collision information.
+"""
+
+class COLLISION(object):
+    """Create COLLISION() object just for sheath model."""
+    
+    def __init__(self, name='Collision'):
+        """
+        Init the COLLISION.
+        
+        name:str, name of the collision.
+        """
+        self.name = name
+
+    def add_func_CollFreq(self, func_CollFreq):
+        """
+        Add func of collision frquency.
+        
+        func_CollFreq: function of energy, f(energy) returns collision frequency.
+        """
+        self.func_CollFreq = func_CollFreq
+    
+    def add_func_ReinitVel(self, func_ReinitVel):
+        """
+        Add func of reinit velocity.
+        
+        func_ReinitVel: function of xxx, f(xxx) returns velocity.
+        """
+        self.func_ReinitVel = func_ReinitVel
