4cdafd2c27dfd8a7a524d9e4a0e6ad2f432bcad1
buckees <67809187+buckees@users.noreply.github.com>
2021-02-19 11:06:04 -0800

    Add xFunc(), vFunc() and oper.idiag

--- run/Feature2D/Feature2D_run.py
+++ run/Feature2D/Feature2D_run.py
@@ -1,5 +1,9 @@
 """Feature Model 2D. Main program."""
 
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
 from packages.Model.Common.Yaml import PARAMETER
 from packages.Model.Common.Particle import PARTICLE
 from packages.Model.Feature2D.Feature2D_mesh import MESH2D
@@ -17,6 +21,8 @@ oper.surf_norm_range = 3
 oper.surf_norm_mode = 'Sum Vector'
 oper.num_plot = 5
 oper.prob_rflct = 0.5
+oper.idiag = True
+
 
 # init mesh
 mesh = MESH2D()
@@ -28,6 +34,18 @@ mesh.readin_mesh(fname)
 ptcl = PARTICLE()
 ptcl.customize_ptcl('Ion', 40, 1)
 
+def xFunc():
+    return mesh.init_ptcl_posn()
+
+vel_list = np.load('vel.npy')
+num = len(vel_list)
+def vFunc():
+    idx = random.randint(0, num-1)
+    return vel_list[idx]
+
+ptcl.add_initPosnFunc(xFunc)
+ptcl.add_initVelFunc(vFunc)
+
 # init reflection
 rflct = REFLECT()
 
@@ -35,3 +53,40 @@ rflct = REFLECT()
 rct = REACT()
 
 MAIN(oper, ptcl, mesh, rct, rflct)
+
+if oper.idiag:
+    vel = np.load('Feat2D_initVel.npy')
+    
+    fig, axes = plt.subplots(2, 2, figsize=(12, 8), dpi=600,
+                               constrained_layout=True)
+    
+    ax = axes[0, 0]
+    ax.hist2d(vel[:, 0], vel[:, 1], bins=200, density=False)
+    ax.set_title('Velocity Distribution')
+    ax.set_xlabel('Vx (m/s)')
+    ax.set_ylabel('Vz (m/s)')
+    
+    ax = axes[0, 1]
+    ax.hist2d(vel[:, 0], vel[:, 1], bins=200, density=False)
+    ax.set_title('Velocity Distribution')
+    ax.set_xlabel('Angle (degree)')
+    ax.set_ylabel('Energy (eV)')
+    
+    ax = axes[1, 0]
+    ax.hist(vel[:, 0], bins=100, density=False)
+    ax.set_title('Ion Energy Distribution')
+    ax.set_xlabel('Energy (eV)')
+    ax.set_ylabel('Count')
+    # ax.set_xlim([0, 200])
+    
+    ax = axes[1, 1]
+    ax.hist(vel[:, 1], bins=100, density=False)
+    ax.set_title('Ion Angular Distribution')
+    ax.set_xlabel('Angle (degree)')
+    ax.set_ylabel('Count')
+    # ax.set_xlim([-4, 4])
+    
+    fpng = 'init_distrb'
+    fig.savefig(fpng + '.png', dpi=600)
+    plt.close()
+    
