37e76c29439c0e64147ad50b6a001001f9cc53b9
buckees <67809187+buckees@users.noreply.github.com>
2021-02-24 17:25:36 -0800

    Create Feature2D_stats.py

---
+++ packages/Model/Feature2D/Feature2D_stats.py
@@ -0,0 +1,15 @@
+"""Diagnose the feature model."""
+
+class STATS(object):
+    """Mesh object."""
+
+    def __init__(self, name='Chemistry'):
+        """
+        Init the MESH2D.
+        
+        name: str, var, name of the CHEMISTRY.
+        """
+        self.species = dict()
+        self.vel = dict()
+        self.erg = dict()
+        self.ang = dict()
